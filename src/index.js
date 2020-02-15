a3fb271d6e883da63eb15d40119dbdf066755ae0
James Mudidi <mudidi.jimmy@gmail.com>
2020-02-15 21:34:32 +0300

    deploy to heroku

--- src/index.js
+++ src/index.js
@@ -1,6 +1,7 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import Video from './components/Video';
+import * as serviceWorker from './serviceWorker';
 
 
 ReactDOM.render(
@@ -9,3 +10,8 @@ ReactDOM.render(
   </div>,
   document.getElementById('root')
 );
+
+// If you want your app to work offline and load faster, you can change
+// unregister() to register() below. Note this comes with some pitfalls.
+// Learn more about service workers: https://bit.ly/CRA-PWA
+serviceWorker.unregister();
